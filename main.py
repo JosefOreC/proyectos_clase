02849587b70400519394bb7c6ac649b47e9181bb
jOre <71660137@continental.edu.pe>
2025-03-26 11:26:23 -0500

    EJERCICIO 01

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 lista_numeros=[int(input(f"Ingrese el {i+1}° número: ")) for i in range(3)]
 
-#mayor_numero=lista_numeros[0]
-#for i in range(1,len(lista_numeros)):
-#   if mayor_numero<lista_numeros[i]: mayor_numero = lista_numeros[i]
+mayor_numero=lista_numeros[0]
+for i in range(1,len(lista_numeros)):
+   if mayor_numero<lista_numeros[i]: mayor_numero = lista_numeros[i]
 
-mayor_numero= max(lista_numeros)
+#mayor_numero= max(lista_numeros)
 
 print("Lista numeros: ",end=' ')
 for i in lista_numeros: print(i, end=" ")
